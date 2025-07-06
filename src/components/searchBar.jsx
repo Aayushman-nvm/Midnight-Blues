3d2396d6249b0a7ac36140b1f4c79afcc873c6f0
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-06 16:25:59 +0530

    Updated search bar... removed animation

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -18,10 +18,10 @@ function SearchBar() {
     }
 
     return (
-        
+
         <div className="relative w-full">
 
-            <div className={`relative z-10 flex items-center bg-gray-800 rounded-full transition-all duration-300 ${isFocused ? 'ring-2 ring-purple-500' : ''}`}>
+            <div className={`border border-white/30 hover:border-white relative z-10 flex items-center bg-gray-950 hover:bg-gray-900 rounded-full transition-all duration-300 ${isFocused ? 'ring-2 ring-white/50' : ''}`}>
                 <input
                     type="text"
                     value={searchParam}
@@ -30,12 +30,12 @@ function SearchBar() {
                     onFocus={() => setIsFocused(true)}
                     onBlur={() => setIsFocused(false)}
                     placeholder="Search artists, albums, tracks..."
-                    className="w-full bg-transparent text-white px-4 py-2 rounded-full focus:outline-none placeholder-gray-400"
+                    className="w-full bg-transparent text-white px-4 my-2 focus:outline-none border-r-2 border-r-white/30 placeholder-gray-400 font-inter"
                 />
                 <button
                     type="submit"
                     onClick={handleRedirect}
-                    className="bg-purple-600 hover:bg-purple-700 text-white p-2 rounded-full mr-1 transition-colors duration-200 flex items-center justify-center"
+                    className="bg-gray-950 hover:bg-gray-900 hover:text-white text-white/40 p-2 rounded-full mx-1 transition-colors duration-200 flex items-center justify-center"
                 >
                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
@@ -43,7 +43,6 @@ function SearchBar() {
                 </button>
             </div>
 
-            <div className="absolute -inset-0.5 bg-gradient-to-r from-purple-600 to-blue-500 rounded-full blur opacity-30 animate-pulse z-0"></div>
         </div>
 
     );
